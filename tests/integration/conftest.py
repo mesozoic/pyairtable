be767148e26f9276d306f8e2078b4ac3ff64f968
Alex Levy <mesozoic@users.noreply.github.com>
2023-05-27 22:28:19 -0700

    Clean up api_key/base_id in integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,7 +4,37 @@ import pytest
 
 from pyairtable import Base, Table
 
-BASE_ID = "appaPqizdsNHDvlEm"
+
+@pytest.fixture
+def api_key():
+    return os.environ["AIRTABLE_API_KEY"]
+
+
+@pytest.fixture
+def create_meta(api_key, base_id):
+    """
+    This fixture is a callable that returns a Meta class with a real API key.
+
+    ``create_meta(table)`` uses the ``base_id`` fixture by default.
+    ``create_meta(base, table)`` only uses the ``api_key`` fixture.
+    """
+
+    def _create_meta(*args):
+        if len(args) == 1:
+            _base_id, table_name = base_id, args[0]
+        elif len(args) == 2:
+            _base_id, table_name = args
+        else:
+            raise ValueError("create_meta() takes 1 (table) or 2 (base, table) args")
+        cfg = {"api_key": api_key, "base_id": _base_id, "table_name": table_name}
+        return type("Meta", (), cfg)
+
+    return _create_meta
+
+
+@pytest.fixture
+def base_id():
+    return "appaPqizdsNHDvlEm"
 
 
 @pytest.fixture
@@ -33,9 +63,8 @@ def cols():
 
 
 @pytest.fixture
-def base():
-    base_id = BASE_ID
-    base = Base(os.environ["AIRTABLE_API_KEY"], base_id)
+def base(api_key, base_id):
+    base = Base(api_key, base_id)
     yield base
     table_name = "TEST_TABLE"
     records = base.all(table_name)
@@ -43,10 +72,9 @@ def base():
 
 
 @pytest.fixture
-def table():
-    base_id = BASE_ID
+def table(api_key, base_id):
     table_name = "TEST_TABLE"
-    table = Table(os.environ["AIRTABLE_API_KEY"], base_id, table_name)
+    table = Table(api_key, base_id, table_name)
     yield table
     records = table.all()
     table.batch_delete([r["id"] for r in records])
